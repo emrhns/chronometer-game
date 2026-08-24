b7687ba49cf332911553d9b1741bfab11c1572e1
Emirhan Şen <emirhansen@Emirhan-MacBook-Air-3.local>
2026-08-24 10:18:53 +0300

    Change game target to 60 seconds

--- src/supabase.js
+++ src/supabase.js
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zhkadivgxavkhbxfrmbn.supabase.co";
 
-const supabaseKey =
-  "sb_publishable_y6cafsOT9x_-OXhPWDkl7Q_fRHIH8zf";
+const supabaseKey = "sb_publishable_y6cafsOT9x_-OXhPWDkl7Q_fRHIH8zf";
 
 export const supabase = createClient(
   supabaseUrl,
